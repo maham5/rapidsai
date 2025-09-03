4ab69e92f0dbf156ad6f46f19d1d7568dd719f7d
Maham-Fatima <137761510+maham5@users.noreply.github.com>
2025-09-03 18:27:20 +0500

    commit1

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import Section4 from '@/components/sections/home1/Section4'
 import Section5 from '@/components/sections/home1/Section5'
 import Section6 from '@/components/sections/home1/Section6'
 import Section7 from '@/components/sections/home1/Section7'
-import Section8 from '@/components/sections/home1/Section8'
+// import Section8 from '@/components/sections/home1/Section8'
 // import Section9 from '@/components/sections/home1/Section9'
 export default function Home() {
 
@@ -18,7 +18,7 @@ export default function Home() {
 			<Layout headerStyle={1} >
 				<Section1 />
 				<Section2 />
-				<Section8 />
+				{/* <Section8 /> */}
 				<Section3 />
 				<Section4 />
 				<Section5 />
